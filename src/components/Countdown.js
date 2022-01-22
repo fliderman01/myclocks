6fb2e0c4eef1bb10c9914aee048db387ae71983b
fliderman01 <fliderman01@gmail.com>
2022-01-22 19:10:32 +0400

    added stopwatch & countdown - 2 files modified

--- src/components/Countdown.js
+++ src/components/Countdown.js
@@ -1,42 +1,127 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../style.css';
 import './Countdown.css';
 
 export default function Countdown() {
+  const [showInp, setShowInp] = useState(true);
+  const [sec, setSec] = useState(0);
+  const [min, setMin] = useState(0);
+  const [hour, setHour] = useState(0);
+  const [isActive, setIsActive] = useState(false);
+  const [isPaused, setIsPaused] = useState(true);
+  // SET HOURS, MINUTES, SECONDS
+  const changeHour = (e) => {
+    setHour(e.target.value);
+  };
+  const changeMin = (e) => {
+    setMin(e.target.value);
+  };
+  const changeSec = (e) => {
+    setSec(e.target.value);
+  };
+  // PAUSE, START & EDIT INPUT
+  const pause = () => {
+    setIsActive(false);
+    setIsPaused(true);
+  };
+  const start = () => {
+    setIsActive(true);
+    setIsPaused(false);
+    setShowInp(false);
+  };
+  const edit = () => {
+    setIsActive(false);
+    setIsPaused(true);
+    setShowInp(true);
+  };
+
+  useEffect(() => {
+    if (isActive) {
+      var interval = setInterval(() => {
+        setSec(sec - 1);
+        sec == 0 && setMin(min - 1);
+        sec == 0 && setSec(59);
+        min == 0 && setHour(hour - 1);
+        min == 0 && setMin(59);
+        if (hour < 0) {
+          setSec(0);
+          setMin(0);
+          setHour(0);
+          setIsActive(false);
+          setIsPaused(true);
+        }
+      }, 1000);
+      return () => clearInterval(interval);
+    } else if (isPaused) {
+      clearInterval(interval);
+    }
+  });
   return (
-    <>
-      <article className="CDown">
+    <article className="CDown">
+      {showInp ? (
+        <div className="numInputs">
+          <label>
+            Hours
+            <input
+              onChange={changeHour}
+              type="number"
+              min="0"
+              id="hours"
+              value={hour}
+            />
+          </label>
+          <label>
+            Minutes
+            <input
+              onChange={changeMin}
+              type="number"
+              max="59"
+              min="0"
+              id="minutes"
+              value={min}
+            />
+          </label>
+          <label>
+            Seconds
+            <input
+              onChange={changeSec}
+              type="number"
+              max="59"
+              min="0"
+              id="seconds"
+              value={sec}
+            />
+          </label>
+        </div>
+      ) : (
         <div className="clock">
-          <p>00</p>
-          <span>:</span>
-          <p>00</p>
-          <span>:</span>
-          <p>00</p>
+          <p>
+            {hour < 10 ? '0' + hour : hour}:{min < 10 ? '0' + min : min}:
+            {sec < 10 ? '0' + sec : sec}
+          </p>
         </div>
-        <label htmlFor="countdown">
-          <input type="text" name="countdown" id="countdown" />
-        </label>
-        <div className="CDBtns">
-          <button>
-            <img
-              src="https://www.freeiconspng.com/uploads/restart-icon-9.png"
-              width="44px"
-            />
-          </button>
-          <button>
-            <img
-              src="https://cdn.iconscout.com/icon/free/png-256/start-1438842-1214529.png"
-              width="44px"
-            />
-          </button>
-          <button>
-            <img
-              src="https://www.freeiconspng.com/uploads/pause-icon-13.png"
-              width="44px"
-            />
-          </button>
-        </div>
-      </article>
-    </>
+      )}
+
+      <div className="CDBtns">
+        <button onClick={start}>
+          <img
+            src="https://cdn.iconscout.com/icon/free/png-256/start-1438842-1214529.png"
+            width="44px"
+          />
+        </button>
+        <button onClick={pause}>
+          <img
+            src="https://www.freeiconspng.com/uploads/pause-icon-13.png"
+            width="44px"
+          />
+        </button>
+        <button onClick={edit}>
+          <img
+            src="https://www.freeiconspng.com/uploads/edit-new-icon-22.png"
+            width="44px"
+          />
+        </button>
+      </div>
+    </article>
   );
 }
--- src/components/Stopwatch.js
+++ src/components/Stopwatch.js
@@ -1,35 +1,69 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../style.css';
 import './Stopwatch.css';
 
 export default function Stopwatch() {
+  const [sec, setSec] = useState(0);
+  const [min, setMin] = useState(0);
+  const [hour, setHour] = useState(0);
+  const [isActive, setIsActive] = useState(false);
+  const [isPaused, setIsPaused] = useState(true);
+
+  const pause = () => {
+    setIsActive(false);
+    setIsPaused(true);
+  };
+  const start = () => {
+    setIsActive(true);
+    setIsPaused(false);
+  };
+
+  const resetT = () => {
+    setSec(0);
+    setMin(0);
+    setHour(0);
+    setIsPaused(true);
+    setIsActive(false);
+  };
+
+  useEffect(() => {
+    if (isActive) {
+      var interval = setInterval(() => {
+        setSec(sec + 1);
+        sec == 59 && setMin(min + 1);
+        sec == 59 && setSec(0);
+        min == 59 && setHour(hour + 1);
+        min == 59 && setMin(0);
+      }, 1000);
+      return () => clearInterval(interval);
+    } else if (isPaused) {
+      clearInterval(interval);
+    }
+  });
+
   return (
     <>
       <article className="StWatch">
         <div className="clock">
-          <p>00</p>
-          <span>:</span>
-          <p>00</p>
-          <span>:</span>
-          <p>00</p>
+          <p>
+            {hour < 10 ? '0' + hour : hour}:{min < 10 ? '0' + min : min}:
+            {sec < 10 ? '0' + sec : sec}
+          </p>
         </div>
-        <label htmlFor="stopwatch">
-          <input type="text" name="stopwatch" id="stopwatch" />
-        </label>
         <div className="StWatchBtns">
-          <button>
+          <button onClick={resetT}>
             <img
               src="https://www.freeiconspng.com/uploads/restart-icon-9.png"
               width="44px"
             />
           </button>
-          <button>
+          <button onClick={start}>
             <img
               src="https://cdn.iconscout.com/icon/free/png-256/start-1438842-1214529.png"
               width="44px"
             />
           </button>
-          <button>
+          <button onClick={pause}>
             <img
               src="https://www.freeiconspng.com/uploads/pause-icon-13.png"
               width="44px"
