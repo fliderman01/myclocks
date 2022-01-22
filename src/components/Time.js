3e7d3d5fd5b826f18fe4fca9773b0126918848f3
fliderman01 <fliderman01@gmail.com>
2022-01-22 15:07:06 +0400

    added Time - 1 file modified

--- src/components/Time.js
+++ src/components/Time.js
@@ -1,25 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../style.css';
 import './Time.css';
 
 export default function Time() {
+  const [time, setTime] = useState();
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const date = new Date();
+      setTime(date.toLocaleTimeString());
+    }, 1000);
+    return () => clearInterval(interval);
+  });
   return (
-    <>
-      <article className="Time">
-        <div className="clock">
-          <p>00</p>
-          <span>:</span>
-          <p>00</p>
-          <span>:</span>
-          <p>00</p>
-        </div>
-        <label>
-          <select name="time" id="time">
-            <option value="Tbilisi">Tbilisi</option>
-            <option value="London">London</option>
-          </select>
-        </label>
-      </article>
-    </>
+    <article className="Time">
+      <p className="clock">{time}</p>
+    </article>
   );
 }
