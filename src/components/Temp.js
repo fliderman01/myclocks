2f3c36fff1734c4820c162a797222ffd5bc53da8
fliderman01 <fliderman01@gmail.com>
2022-01-24 14:47:21 +0400

    added temperature - 1 file modified

--- src/components/Temp.js
+++ src/components/Temp.js
@@ -1,19 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../style.css';
 import './Temp.css';
 
 export default function Temp() {
+  const [displ, setDispl] = useState('');
+  const [cityN, setCityN] = useState(() => {
+    const saved = localStorage.getItem('temp');
+    return saved ? JSON.parse(saved) : `Tbilisi`;
+  });
+
+  useEffect(() => {
+    localStorage.setItem('temp', JSON.stringify(cityN));
+  }, [cityN]);
+  // fetch data from openweathermap.org
+  fetch(
+    'https://api.openweathermap.org/data/2.5/weather?q=' +
+      cityN +
+      ',&appid=822fc8446f5adc72ac8c766a871329a8'
+  )
+    .then((res) => {
+      return res.json();
+    })
+    .then((res) => {
+      setDispl(
+        `${Math.round(res.main.temp - 273.15)}°C, ${res.name},${
+          res.sys.country
+        }; "${res.weather[0].description}"`
+      );
+    });
+  // upload city name
+  const uplCity = (e) => {
+    e.charCode == 13 && setCityN(e.target.value);
+  };
   return (
     <>
       <article className="Temp">
-        <p>
-          Tbilisi: 17 <sup>O</sup>; Clear Sky
-        </p>
+        <p>{displ}</p>
         <label>
-          <select name="temp" id="temp">
-            <option value="Tbilisi">Tbilisi</option>
-            <option value="London">London</option>
-          </select>
+          <input
+            type="text"
+            placeholder="Enter city name..."
+            onKeyPress={uplCity}
+          />
         </label>
       </article>
     </>
