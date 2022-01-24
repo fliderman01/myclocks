7c2e0cf84408136812589082867aaacb5402e523
fliderman01 <fliderman01@gmail.com>
2022-01-24 15:43:37 +0400

    1 file modified

--- src/components/Countdown.js
+++ src/components/Countdown.js
@@ -41,6 +41,7 @@ export default function Countdown() {
           setSec(0);
           setIsActive(false);
           setIsPaused(true);
+          // play music
         }
       }, 1000);
       return () => clearInterval(interval);
@@ -93,6 +94,16 @@ export default function Countdown() {
           </p>
         </div>
       )}
+      {/* <iframe
+        style={{ display: 'none' }}
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/DJHcZyyuj_8?autoplay=1"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      ></iframe> */}
 
       <div className="CDBtns">
         <button onClick={start}>
