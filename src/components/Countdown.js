c43a2b6edc8a540425595d1e95be705801fb81e2
fliderman01 <fliderman01@gmail.com>
2022-01-24 20:49:15 +0400

    deleted audio in countdown - 2 files modified

--- src/components/Countdown.js
+++ src/components/Countdown.js
@@ -41,7 +41,6 @@ export default function Countdown() {
           setSec(0);
           setIsActive(false);
           setIsPaused(true);
-          // play music
         }
       }, 1000);
       return () => clearInterval(interval);
@@ -94,16 +93,6 @@ export default function Countdown() {
           </p>
         </div>
       )}
-      {/* <iframe
-        style={{ display: 'none' }}
-        width="560"
-        height="315"
-        src="https://www.youtube.com/embed/DJHcZyyuj_8?autoplay=1"
-        title="YouTube video player"
-        frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowfullscreen
-      ></iframe> */}
 
       <div className="CDBtns">
         <button onClick={start}>
